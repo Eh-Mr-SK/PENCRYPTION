0b7031688edd1654eadd43b89dfda075fbbd9150
Eh-Mr-SK <sumitkhobragade695@gmail.com>
2022-04-20 01:36:39 +0530

    Initial commit

---
+++ core/bdata.py
@@ -0,0 +1,45 @@
+"""
+    author:Mr_Sk
+    
+
+    abstract ByteData class that can be converted from/to bytes.
+"""
+
+from abc import ABC, abstractmethod
+from .bstream import ByteStream
+
+class ByteData(ABC):
+    """
+    abstract class to represent types that use bytes.
+    """
+    @classmethod
+    @abstractmethod
+    def read(cls, data_stream: ByteStream):
+        """
+        Read bytes from byte stream to construct the class.
+        """
+        ...
+
+    @abstractmethod
+    def write(self, data_stream: ByteStream) -> None:
+        """
+        write bytes to byte stream.
+        """
+        ...
+
+    @classmethod
+    def from_bytes(cls, data: bytes, offset: int = 0) -> 'ImageHeader':
+        """
+        Load header from bytes.
+        """
+        data_stream = ByteStream(data)
+        data_stream.seek(offset)
+        return cls.read(data_stream)
+
+    def to_bytes(self) -> bytes:
+        """
+        Get bytes of header
+        """
+        data_stream = ByteStream()
+        self.write(data_stream)
+        return data_stream.getvalue()
